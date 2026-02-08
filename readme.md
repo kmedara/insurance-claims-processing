abb179ee47b3304b187e3bccf8145d58e2611f89
Kevin Medara <kmedara2015@outlook.com>
2026-02-08 01:08:58 -0500

    processing wip

--- readme.md
+++ readme.md
@@ -19,11 +19,24 @@ Core logic for an insurance claims processing system. The system evaluates incom
 
 ## How to use
 
+upon start, the example claim will be evaluated and the result will be output to the console
+
 ## Decisions
 
 - debated on strategy pattern or rule orchestration
   - Chose rule orchestration because logic is highly conditional and may overlap across strategies
   - Algorithms do not differ structurally
   - Frequent changes to rules are easier to manage
+  - rules have no dependency, just react based on context
 
 ## Enhancements
+
+- Allow rules to be applied conditionally
+- allow ordering and rule priority
+- user interface
+- higher abstraction level for rules
+  build rulesets conditionally?
+- seperate I/O, data processing, and data access layers
+- progress updates/events
+- dedicated rules engine class and supporting types
+- Integration testing and testing the rule pipeline
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,42 @@
+import { exampleClaim, examplePolicies } from "./data.js";
+import {
+  payoutMustBeGreaterThanZero,
+  payoutMustBeLessThanCoverageLimit,
+  policyCoversIncident,
+  policyIsActiveOnIncidentDate,
+  simplePayout,
+} from "./rules/rules.js";
+import type { EvaluationResult, RuleSet } from "./types.js";
+
+export const baseRuleSet: RuleSet = [
+  policyIsActiveOnIncidentDate,
+  policyCoversIncident,
+  payoutMustBeGreaterThanZero,
+  payoutMustBeLessThanCoverageLimit,
+];
+
+export const payoutCalculations: RuleSet = [simplePayout];
+
 async function main() {
-  console.log("hello world");
+  console.log("processing...");
+  const claim = exampleClaim;
+  const policy = examplePolicies.find((p) => p.policyId === claim.policyId)!;
+  var result: EvaluationResult = {
+    approved: true,
+    payout: 0,
+    reasonCode: "APPROVED",
+  };
+
+  //   payoutCalculations.reduce((_, curr) => {
+  //     console.log("reducing");
+  //     curr(claim, result, policy);
+  //     return curr;
+  //   });
+
+  payoutCalculations.forEach((element) => {
+    element(claim, result, policy);
+  });
+  console.log(result);
 }
 
 await main();
